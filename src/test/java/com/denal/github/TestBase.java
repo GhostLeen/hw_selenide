c544652dc2fa4a14556e1bf19b5ca953c25e54b2
Alena D <denyushina.alena@gmail.com>
2022-12-29 09:06:50 +0300

    set for remote launch

--- src/test/java/com/denal/github/TestBase.java
+++ src/test/java/com/denal/github/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
